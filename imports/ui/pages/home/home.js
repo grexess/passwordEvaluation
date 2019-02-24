b32ab367548aad8ec420b15bca1d3d4cd6c7fe3d
grexess <grexess@googlemail.com>
2019-02-24 19:56:39 +0100

    new change

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -11,42 +11,64 @@ import '../../components/links.js';
 
 Template.App_home.events({
 
-    'input #password'(event, template) {
-        Session.set("password", event.currentTarget.value);
-    }
+  'input #password'(event, template) {
+    Session.set("password", event.currentTarget.value);
+  },
+
+  'input .w3-check'(event, template) {
+    //activate input if at least one character is set selected
+    $("#password").prop('disabled', $(".w3-check:checked").length === 0);
+
+    checkCharacterAmount();
+
+  }
+
+
 
 });
 
 Template.App_home.helpers({
 
-    length() {
-        return Session.get( "password" ).length;
-    },
+  length() {
+    return Session.get("password").length;
+  },
 
-    permcount() {
-        return permutationCount(Session.get( "password" ).length,Session.get( "password" ).length) ;
-    }
+  permcount() {
+    return permutationCount(Session.get("password").length, Session.get("characterAmount"));
+  },
+
+  characterAmount() {
+    return Session.get("characterAmount");
+  }
 
 })
 
+/* check which character set is selected and display combinations*/
+function checkCharacterAmount() {
 
-function permutationCount(n, k) {
-    var result = 1;
-  
-    var divisor = factorial(n - k);
-    if (divisor) {
-      result = factorial(n) / divisor;
-    }
-  
-    return result;
+  amount = 0;
+
+  if($("#yNumbers").prop('checked')){
+    amount = amount + 10;
+  }
+  if($("#ysmallChar").prop('checked')){
+    amount = amount + 26;
+  }
+  if($("#ybigChar").prop('checked')){
+    amount = amount + 26;
+  }
+  if($("#yspecialChar").prop('checked')){
+    amount = amount + 33;
+  }
+  if($("#ygermanUmlaut").prop('checked')){
+    amount = amount + 7;
   }
 
-  var factorial = function(n) {
-    var result = 1;
-  
-    for (var i=n; i>0; i--) {
-      result *= i;
-    }
-  
-    return result;
-  }
+  Session.set("characterAmount", amount);
+
+}
+
+function permutationCount(length, amount) {
+
+  return Math.pow(amount,length);
+}
